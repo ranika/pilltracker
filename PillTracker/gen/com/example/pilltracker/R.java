8e977a4b473e545f58139b9259bc1e06bfc385fb
ranikakejriwal <ranika.kejriwal@gmail.com>
2014-04-09 23:12:38 -0400

    uml for mainactivity
    main activity is the home screen

--- PillTracker/gen/com/example/pilltracker/R.java
+++ PillTracker/gen/com/example/pilltracker/R.java
@@ -10,6 +10,9 @@ package com.example.pilltracker;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,23 +25,45 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrow=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f090010;
+        public static final int button1=0x7f090009;
+        public static final int button2=0x7f09000a;
+        public static final int button3=0x7f09000b;
+        public static final int button4=0x7f09000c;
+        public static final int button5=0x7f09000d;
+        public static final int button6=0x7f09000e;
+        public static final int button7=0x7f09000f;
+        public static final int container=0x7f090000;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090002;
+        public static final int textView3=0x7f090003;
+        public static final int textView4=0x7f090004;
+        public static final int textView5=0x7f090005;
+        public static final int textView6=0x7f090006;
+        public static final int textView7=0x7f090007;
+        public static final int view1=0x7f090008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int doctors=0x7f050005;
+        public static final int med_log=0x7f050002;
+        public static final int notes=0x7f050007;
+        public static final int pharmacies=0x7f050006;
+        public static final int prescriptions=0x7f050003;
+        public static final int reminders=0x7f050004;
+        public static final int settings=0x7f050008;
     }
     public static final class style {
         /** 
@@ -62,10 +87,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
