15225f8d89dacecfd8ab8238a65fad8866e250cb
ranikakejriwal <ranika.kejriwal@gmail.com>
2014-04-10 00:23:18 -0400

    uml for prescriptions

--- PillTracker/gen/com/example/pilltracker/R.java
+++ PillTracker/gen/com/example/pilltracker/R.java
@@ -59,17 +59,21 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_med_log=0x7f030001;
-        public static final int fragment_main=0x7f030002;
-        public static final int fragment_med_log=0x7f030003;
+        public static final int activity_prescription=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int fragment_med_log=0x7f030004;
+        public static final int fragment_prescription=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
         public static final int med_log=0x7f080001;
+        public static final int prescription=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int doctors=0x7f050005;
+        public static final int hello_world=0x7f050011;
         public static final int med_log=0x7f050002;
         public static final int nasal=0x7f05000f;
         public static final int notes=0x7f050007;
@@ -81,6 +85,7 @@ public final class R {
         public static final int ten_am=0x7f05000b;
         public static final int ten_pm=0x7f05000e;
         public static final int title_activity_med_log=0x7f050009;
+        public static final int title_activity_prescription=0x7f050010;
         public static final int today=0x7f05000a;
         public static final int vitamin=0x7f05000d;
     }
